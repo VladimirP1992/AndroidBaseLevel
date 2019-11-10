99f1adbc80215b852b263571b9eaa9ffcc0906b0
VladimirP1992 <p.vladimir.m@yandex.ru>
2019-11-10 18:37:32 +0300

    Выгразка сохраненных данных в OnCreate() и сохранение в обработчиках (в singleton)

--- app/src/main/java/ru/geekbrains/androidBase/lesson1/CitySelectionActivity.java
+++ app/src/main/java/ru/geekbrains/androidBase/lesson1/CitySelectionActivity.java
@@ -2,12 +2,16 @@ package ru.geekbrains.androidBase.lesson1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
-public class CitySelectionActivity extends AppCompatActivity implements View.OnClickListener {
+public class CitySelectionActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,13 +19,66 @@ public class CitySelectionActivity extends AppCompatActivity implements View.OnC
         setContentView(R.layout.activity_city_selection);
 
         Button backButton = findViewById(R.id.backButton);
-        backButton.setOnClickListener(this);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBack();
+            }
+        });
+
+        //Lesson 3 - exercise 4
+        //create singleton
+        final CitySelectionSingleton presenter = CitySelectionSingleton.getInstance();
+
+        //find all views that should be saved
+        final AutoCompleteTextView cityNameField = findViewById(R.id.cityAutoCompleteTextView);
+        Switch windSwitch = findViewById(R.id.windSwitch);
+        Switch pressureSwitch = findViewById(R.id.pressureSwitch);
+
+        //get saved information from singleton
+        cityNameField.setText(presenter.getCityFieldText());
+        windSwitch.setChecked(presenter.isWindSwitchChecked());
+        pressureSwitch.setChecked(presenter.isPressureSwitchChecked());
+
+        //Save changes
+//        cityNameField.setOnHoverListener(new View.OnHoverListener() {
+//            @Override
+//            public boolean onHover(View v, MotionEvent event) {
+//                presenter.setCityFieldText(cityNameField.getText().toString());
+//                return false;
+//            }
+//        });
+        cityNameField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                presenter.setCityFieldText(cityNameField.getText().toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        windSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                presenter.setWindSwitchState(isChecked);
+            }
+        });
+        pressureSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                presenter.setPressureSwitchState(isChecked);
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-        Intent intent;
-        intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+    public void onBack(){
+        finish();
     }
 }
