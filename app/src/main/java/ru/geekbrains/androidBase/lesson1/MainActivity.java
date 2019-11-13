393ed85481b32618ebf65450e316b5e5ece077c8
VladimirP1992 <p.vladimir.m@yandex.ru>
2019-11-13 18:25:43 +0300

    Выгрузка данных из активити выбора города и кнопка выхода в интернет

--- app/src/main/java/ru/geekbrains/androidBase/lesson1/MainActivity.java
+++ app/src/main/java/ru/geekbrains/androidBase/lesson1/MainActivity.java
@@ -1,18 +1,23 @@
 package ru.geekbrains.androidBase.lesson1;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements ConstantNames{
 
     private static final String TAG = "Lesson3";
+    private static final int REQUEST_CODE = 13579;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +39,16 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
+        //Lesson 4 - exercise 2
+        Button browserButton = findViewById(R.id.browserButton);
+        browserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBrowserActivity();
+            }
+        });
+
+
         Toast.makeText(getApplicationContext(), "onCreate()", Toast.LENGTH_SHORT).show();
         Log.d(TAG,"onCreate()");
 
@@ -42,7 +57,7 @@ public class MainActivity extends AppCompatActivity{
     public void openCitySelectActivity(){
         Intent intent;
         intent = new Intent(this, CitySelectionActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, REQUEST_CODE);
     }
 
     public void openSettingsActivity(){
@@ -51,6 +66,44 @@ public class MainActivity extends AppCompatActivity{
         startActivity(intent);
     }
 
+    public void openBrowserActivity(){
+        final TextView cityNameTextView = findViewById(R.id.cityNameTextView);
+        String url = "https://yandex.ru/pogoda/" + cityNameTextView.getText().toString();
+        Uri uri = Uri.parse(url);
+
+        Intent browser = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(browser);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        //Lesson 4 - exercise 1
+        if(requestCode != REQUEST_CODE) {
+            super.onActivityResult(requestCode, resultCode, data);
+            return;
+        }
+        if(resultCode == RESULT_OK){
+            TextView cityNameTextView = findViewById(R.id.cityNameTextView);
+            LinearLayout windInfo = findViewById(R.id.windInfoLinearLayout);
+            LinearLayout pressureInfo = findViewById(R.id.pressureInfoLinearLayout);
+
+            CitySelectionInfoParcel additionalInfo = (CitySelectionInfoParcel) data.getExtras().getSerializable(ADDITIONAL_INFO);
+            cityNameTextView.setText(additionalInfo.getCityName());
+            if(additionalInfo.isWindChecked()){
+                windInfo.setVisibility(View.VISIBLE);
+            }else {
+                windInfo.setVisibility(View.GONE);
+            }
+            if(additionalInfo.isPressureChecked()){
+                pressureInfo.setVisibility(View.VISIBLE);
+            }
+            else {
+                pressureInfo.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    //==========Life cycle outputs===========================================================================
     @Override
     protected void onStart() {
         super.onStart();
