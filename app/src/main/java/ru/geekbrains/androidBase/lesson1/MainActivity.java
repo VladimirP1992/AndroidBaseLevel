72c59e91782b3b44a27f9c9109e929b0c4c9a7c0
VladimirP1992 <p.vladimir.m@yandex.ru>
2019-12-01 12:08:09 +0300

    Разбил main_activity на AppBar и включаемый контент (content_main)

--- app/src/main/java/ru/geekbrains/androidBase/lesson1/MainActivity.java
+++ app/src/main/java/ru/geekbrains/androidBase/lesson1/MainActivity.java
@@ -1,7 +1,9 @@
 package ru.geekbrains.androidBase.lesson1;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,6 +12,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -28,29 +32,14 @@ public class MainActivity extends AppCompatActivity implements ConstantNames, We
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
 
         Button citySelectButton = findViewById(R.id.citySelectButton);
         citySelectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openCitySelectActivity();
-            }
-        });
-
-        Button settingsButton = findViewById(R.id.settingsButton);
-        settingsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openSettingsActivity();
-            }
-        });
-
-        //Lesson 4 - exercise 2
-        Button browserButton = findViewById(R.id.browserButton);
-        browserButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openBrowserActivity();
             }
         });
 
@@ -104,31 +93,6 @@ public class MainActivity extends AppCompatActivity implements ConstantNames, We
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         //This logic was moved to this.onResume() and AdditionalInfoFragment.onResume() methods
-
-        //Lesson 4 - exercise 1
-//        if(requestCode != REQUEST_CODE) {
-//            super.onActivityResult(requestCode, resultCode, data);
-//            return;
-//        }
-//        if(resultCode == RESULT_OK){
-//            TextView cityNameTextView = findViewById(R.id.cityNameTextView);
-//            LinearLayout windInfo = findViewById(R.id.windInfoLinearLayout);
-//            LinearLayout pressureInfo = findViewById(R.id.pressureInfoLinearLayout);
-//
-//            CitySelectionInfoParcel additionalInfo = (CitySelectionInfoParcel) data.getExtras().getSerializable(ADDITIONAL_INFO);
-//            cityNameTextView.setText(additionalInfo.getCityName());
-//            if(additionalInfo.isWindChecked()){
-//                windInfo.setVisibility(View.VISIBLE);
-//            }else {
-//                windInfo.setVisibility(View.GONE);
-//            }
-//            if(additionalInfo.isPressureChecked()){
-//                pressureInfo.setVisibility(View.VISIBLE);
-//            }
-//            else {
-//                pressureInfo.setVisibility(View.GONE);
-//            }
-//        }
         super.onActivityResult(requestCode, resultCode, data);
     }
 
@@ -197,6 +161,27 @@ public class MainActivity extends AppCompatActivity implements ConstantNames, We
         super.onDestroy();
         Toast.makeText(getApplicationContext(), "onDestroy()", Toast.LENGTH_SHORT).show();
         Log.d(TAG,"onDestroy()");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int itemId = item.getItemId();
+        if(itemId == R.id.action_settings)
+        {
+            openSettingsActivity();
+        } else if(itemId == R.id.action_yandex_pogoda){
+            openBrowserActivity();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
