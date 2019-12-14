c41c008b13aaf679147b0556df4dc607a875faca
VladimirP1992 <p.vladimir.m@yandex.ru>
2019-12-14 13:42:34 +0300

    Вынес имя файла SharedPreferences и имена полей SharedPreferences в интерфейс с константами

--- app/src/main/java/ru/geekbrains/androidBase/lesson1/ConstantNames.java
+++ app/src/main/java/ru/geekbrains/androidBase/lesson1/ConstantNames.java
@@ -2,4 +2,11 @@ package ru.geekbrains.androidBase.lesson1;
 
 public interface ConstantNames {
     String ADDITIONAL_INFO = "additional_info";
+
+    String CITY_NAME = "cityName";
+    String WIND_SWITCH_STATE = "windSwitchState";
+    String PRESSURE_SWITCH_STATE = "pressureSwitchState";
+    String DARK_THEME_SWITCH_STATE = "darkThemeSwitchState";
+
+    String APP_SHARED_PREFERENCES_NAME = "weatherAppSharedPreferences";
 }
